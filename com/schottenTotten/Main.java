d5baf7df82143ec6cffc19deedcb0f219b4b8190
Mohamed Badra <mbadra001@enseirb-matmeca.fr>
2025-12-08 10:17:46 +0100

    Ajout de l'affichage pour main dans le bon ordre: IA puis joueur

--- com/schottenTotten/Main.java
+++ com/schottenTotten/Main.java
@@ -1,35 +1,68 @@
 package com.schottenTotten;
 
 import com.schottenTotten.controller.Jeu;
-import com.schottenTotten.model.Borne;
-import com.schottenTotten.model.Joueur;
-
-import java.util.*;
+import com.schottenTotten.view.ConsoleView;
+import com.schottenTotten.view.InputView;
+import com.schottenTotten.view.MenuView;
 
 public class Main {
 
     public static void main(String[] args) {
-        Jeu jeu = new Jeu("Joueur 1", "Joueur 2");
+        MenuView menuView = new MenuView();
+        ConsoleView gameView = new ConsoleView();
+
+        menuView.afficherMenuPrincipal();
+
+        // On demande d'abord si on veut jouer contre l'IA
+        boolean contreIA = menuView.demanderAvecIA();
+        
+        String nomJ1 = "";
+        String nomJ2 = "";
+
+        if (contreIA) {
+            // Mode IA
+            nomJ1 = menuView.demanderNomJoueur(1);
+            nomJ2 = "Ordinateur"; // Nom automatique
+            // int niveauIA = menuView.demanderTypeIA(); // pas encore fait 
+        } else {
+
+            // 2 Joueurs
+            nomJ1 = menuView.demanderNomJoueur(1);
+            nomJ2 = menuView.demanderNomJoueur(2);
+        }
+        // 3. Création du jeu (Contrôleur)
+        Jeu jeu = new Jeu(nomJ1, nomJ2, gameView);
         
         boolean partieEnCours = true;
 
+        // 4. Boucle de jeu
         while (partieEnCours) {
+            // On affiche qui doit jouer
+            gameView.afficherMessage("C'est au tour de " + jeu.getJoueurActuel().getNom());
             
-            Joueur joueurActuel = jeu.getJoueurActuel();
-            List<Borne> bornes = jeu.getBornes(); 
-
-            // Affichage du plateau (Les 9 bornes)
+            // On affiche la main du joueur
+            gameView.afficherMain(jeu.getJoueurActuel());
+            
         
-            // Affichage du joueur actif et de sa main
+            int indexCarte = InputView.demanderCarteAJouer();
+            int indexBorne = InputView.demanderBorne();
 
-            // Demande de saisie : CARTE (on obtient indexCarte)
-
-            // Demande de saisie : BORNE (on obtient indexBorne)
-
-            // On joue le tour
-            int indexCarte = 0;
-            int indexBorne = 0;
-            partieEnCours = jeu.jouerTour(indexCarte, indexBorne);
+            
+            try {
+                // jouerTour renvoie false si la partie est finie ou s'il y a problème
+                boolean resultatTour = jeu.jouerTour(indexCarte, indexBorne);
+                
+                // fin de partie 
+                if (!resultatTour) {
+                    // Vérifie si c'est vraiment fini ou juste une erreur
+                   partieEnCours = false;
+                }
+            } catch (Exception e) {
+                gameView.afficherErreur("Coup invalide (mauvais index ?). Réessayez.");
+            }
         }
+        
+    
+        InputView.fermerScanner();
     }
 }
